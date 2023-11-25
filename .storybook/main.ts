cc81cdc5d4381ffcffd62fdcb2ae20044aaa7ae4
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-11-25 11:16:06 -0300

    chore: set up plop

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ['../src/components/**/*.stories.tsx'],
+  stories: ["../src/components/**/*.stories.tsx"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
