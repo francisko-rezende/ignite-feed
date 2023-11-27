c442123bbdcae4bc1d06e934f5871bf8d0c92e72
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-11-27 07:47:19 -0300

    fix: create correct stories for Avatar

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -1,16 +1,21 @@
-import { Meta, StoryObj } from '@storybook/react'
-import { Avatar } from '.'
+import { Meta, StoryObj } from "@storybook/react";
+import { Avatar } from ".";
 
 const meta = {
-  title: 'Avatar',
+  title: "Avatar",
   component: Avatar,
   parameters: {
-    layout: 'centered',
+    layout: "centered",
   },
-} satisfies Meta<typeof Avatar>
+} satisfies Meta<typeof Avatar>;
 
-export default meta
+export default meta;
 
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {}
+export const WithBorder: Story = {
+  args: { hasBorder: true, src: "https://github.com/francisko-rezende.png" },
+};
+export const WithoutBorder: Story = {
+  args: { hasBorder: false, src: "https://github.com/francisko-rezende.png" },
+};
