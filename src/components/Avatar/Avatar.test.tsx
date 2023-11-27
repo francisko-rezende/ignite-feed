f5751541c28200f0e1c2b3c255ddcec526563885
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-11-27 07:39:36 -0300

    test: add test for checking that the correct border class loads in Avatar

--- src/components/Avatar/Avatar.test.tsx
+++ src/components/Avatar/Avatar.test.tsx
@@ -1,13 +1,44 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from "@testing-library/react";
+import styles from "./Avatar.module.css";
+import { Avatar } from ".";
 
-import { Avatar } from '.'
+describe("<Avatar />", () => {
+  it("should render the heading", () => {
+    render(
+      <Avatar
+        hasBorder
+        alt="user avatar"
+        src="https://github.com/francisko-rezende.png"
+      />,
+    );
 
-describe('<Avatar />', () => {
-  it('should render the heading', () => {
-    render(<Avatar />)
+    const avatar = screen.getByAltText(/user avatar/i);
+    expect(avatar).toBeInTheDocument();
+  });
 
-    expect(screen.getByRole('heading', { name: /Avatar/i })).toBeInTheDocument()
+  it("should render the heading with avatarWithBorder class", () => {
+    render(
+      <Avatar
+        hasBorder
+        alt="user avatar"
+        src="https://github.com/francisko-rezende.png"
+      />,
+    );
 
+    const avatar = screen.getByAltText(/user avatar/i);
+    expect(avatar).toHaveClass(styles.avatarWithBorder);
+  });
 
-  })
-})
+  it("should render the heading with avatar class", () => {
+    render(
+      <Avatar
+        hasBorder={false}
+        alt="user avatar"
+        src="https://github.com/francisko-rezende.png"
+      />,
+    );
+
+    const avatar = screen.getByAltText(/user avatar/i);
+    expect(avatar).toHaveClass(styles.avatar);
+  });
+});
