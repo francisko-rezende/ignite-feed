ee5ee081950e9fb1c80795043c2eb65df6c1c5e0
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-11-28 21:39:52 -0300

    fix: add post props to post story

--- src/components/Post/Post.stories.tsx
+++ src/components/Post/Post.stories.tsx
@@ -1,16 +1,39 @@
-import { Meta, StoryObj } from '@storybook/react'
-import { Post } from '.'
+import { Meta, StoryObj } from "@storybook/react";
+import { Post } from ".";
 
 const meta = {
-  title: 'Post',
+  title: "Post",
   component: Post,
   parameters: {
-    layout: 'centered',
+    layout: "centered",
   },
-} satisfies Meta<typeof Post>
+} satisfies Meta<typeof Post>;
 
-export default meta
+export default meta;
 
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {}
+const mockPost = {
+  id: 1,
+  author: {
+    avatarUrl: "https://github.com/francisko-rezende.png",
+    name: "Francisko de Moraes Rezende",
+    role: "Front-end developer",
+  },
+  content: [
+    {
+      type: "paragraph",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ab nobis non tempora qui sit repudiandae. Repudiandae, libero? Inventore sunt voluptatibus reiciendis maxime consectetur quos harum doloremque officia soluta ad.",
+    },
+    {
+      type: "paragraph",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ab nobis non tempora qui sit repudiandae. Repudiandae, libero? Inventore sunt voluptatibus reiciendis maxime consectetur quos harum doloremque officia soluta ad.",
+    },
+    { type: "hashtag", content: "#newproject" },
+  ],
+  publishedAt: new Date("2022-05-03 20:00"),
+};
+
+export const Default: Story = { args: { ...mockPost } };
