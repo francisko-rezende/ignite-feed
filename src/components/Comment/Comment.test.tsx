9effa31fb94ca8025680491197bf98b03be30a97
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-11-29 21:53:53 -0300

    feat: make comments take content as prop

--- src/components/Comment/Comment.test.tsx
+++ src/components/Comment/Comment.test.tsx
@@ -4,14 +4,17 @@ import { Comment } from ".";
 
 describe("<Comment />", () => {
   it("should render the comment with author avatar, name, time, content, and footer with clap button", () => {
-    render(<Comment />);
+    const content = "text comment";
+    render(<Comment content={content} />);
     const avatarElement = screen.getByAltText("comment author avatar");
     const authorElement = screen.getByText(/Francisko de Moraes Rezende/i);
     const timeElement = screen.getByText("Around 1 hour ago");
+    const contentElement = screen.getByText(content);
     const clapButton = screen.getByText("Clap");
 
     expect(avatarElement).toBeInTheDocument();
     expect(authorElement).toBeInTheDocument();
+    expect(contentElement).toBeInTheDocument();
     expect(timeElement).toBeInTheDocument();
     expect(clapButton).toBeInTheDocument();
   });
