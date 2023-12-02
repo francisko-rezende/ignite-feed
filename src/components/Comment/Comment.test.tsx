221f8063521f433a5f4246fff9cf870fe19bacdf
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-12-01 21:28:40 -0300

    feat: implement comment clap

--- src/components/Comment/Comment.test.tsx
+++ src/components/Comment/Comment.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 
 import { Comment } from ".";
+import { renderWithUser } from "../../utils/tests";
 
 describe("<Comment />", () => {
   it("should render the comment with author avatar, name, time, content, and footer with clap button", () => {
@@ -17,5 +18,39 @@ describe("<Comment />", () => {
     expect(contentElement).toBeInTheDocument();
     expect(timeElement).toBeInTheDocument();
     expect(clapButton).toBeInTheDocument();
+  });
+
+  it("should render the comment with 0 claps/likes", () => {
+    const content = "text comment";
+    render(<Comment deleteComment={() => {}} content={content} />);
+
+    const clapButton = screen.getByRole("button", { name: /clap 0/i });
+    expect(clapButton).toBeInTheDocument();
+  });
+
+  it("should increase the comment claps/likes when clicking the clap button", async () => {
+    const content = "text comment";
+    const { user } = renderWithUser(
+      <Comment deleteComment={() => {}} content={content} />,
+    );
+
+    const clapButton = screen.getByRole("button", { name: /clap/i });
+    await user.click(clapButton);
+    const updatedClapButton = screen.getByRole("button", { name: /clap 1/i });
+    expect(updatedClapButton).toBeInTheDocument();
+  });
+
+  it("should increase the comment claps/likes when clicking the clap button by one per click", async () => {
+    const content = "text comment";
+    const { user } = renderWithUser(
+      <Comment deleteComment={() => {}} content={content} />,
+    );
+
+    const clapButton = screen.getByRole("button", { name: /clap/i });
+    await user.click(clapButton);
+    await user.click(clapButton);
+    await user.click(clapButton);
+    const updatedClapButton = screen.getByRole("button", { name: /clap 3/i });
+    expect(updatedClapButton).toBeInTheDocument();
   });
 });
